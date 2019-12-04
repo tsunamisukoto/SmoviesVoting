9c60423bac6717570bc45769f757ac5606542c00
Scott Becker <tsunamisukoto@hotmail.com>
2019-12-04 22:23:17 +1100

    Added some server side. Still need more

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
-import app from './App'
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as helmet from "helmet";
+import * as cors from "cors";
+import routes from "./routes/index";
 
-const port = process.env.PORT || 5053
+//Connects to the Database -> then starts the express
+createConnection()
+  .then(async connection => {
+    // Create a new express application instance
+    const app = express();
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
+    // Call midlewares
+    app.use(cors());
+    app.use(helmet());
+    app.use(bodyParser.json());
 
-  return console.log(`server is listening on ${port}`)
-})
+    //Set all routes from routes folder
+    app.use("/", routes);
+
+    app.listen(3000, () => {
+      console.log("Server started on port 3000!");
+    });
+  })
+  .catch(error => console.log(error));
