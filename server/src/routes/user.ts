a19a58687542bb76846b6891e32474a4a04d294e
Scott Becker <tsunamisukoto@hotmail.com>
2019-12-18 21:48:07 +1100

    removed my previously removed route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -8,13 +8,6 @@ const router = Router();
 // Get all users
 router.get('/', [checkJwt, checkRole(['ADMIN'])], UserController.listAll);
 
-// Get one user
-router.get(
-    '/:id([0-9]+)',
-    [checkJwt, checkRole(['ADMIN'])],
-    UserController.getOneById
-);
-
 // Create a new user
 router.post('/', [checkJwt, checkRole(['ADMIN'])], UserController.newUser);
 
